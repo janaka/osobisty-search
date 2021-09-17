51c7288f3c9e6d26e59bb3c69f94058901677700
Janaka Abeywardhana <contact@janaka.co.uk>
2021-09-17 10:45:39 +0100

    mid building custom search component - the Aligoli components are more complex than what I need. - the customisation effort isn't worth the overhead - API documentation is poor even for JS

--- search-ui/src/App.js
+++ search-ui/src/App.js
@@ -1,93 +1,132 @@
-import React from 'react';
-// import algoliasearch from 'algoliasearch/lite';
-import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
-import {
-  InstantSearch,
-  Hits,
-  SearchBox,
-  Pagination,
-  Highlight,
-} from 'react-instantsearch-dom';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { SearchClient as TypeSearchClient } from "typesense";
+// import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+// import {
+//   InstantSearch,
+//   SearchBox,
+//   Pagination,
+//   Highlight,
+//   connectHits,
+// } from 'react-instantsearch-dom';
 import './App.css';
 
 // 6be0576ff61c053d5f9a3225e2a90f76
 
 // const searchClient = algoliasearch('osobisty-search-ui', 'xyz');
 
-const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
-  server: {
-    apiKey: 'xyz', // Be sure to use the search-only-api-key
-    nodes: [
-      {
-        host: 'localhost',
-        port: '8108',
-        protocol: 'http',
-      },
-    ],
-  },
-  // The following parameters are directly passed to Typesense's search API endpoint.
-  //  So you can pass any parameters supported by the search endpoint below.
-  //  queryBy is required.
-  additionalSearchParameters: {
-    queryBy: 'title',
-  },
+// const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+//   server: {
+//     apiKey: 'xyz', // Be sure to use the search-only-api-key
+//     nodes: [
+//       {
+//         host: 'localhost',
+//         port: '8108',
+//         protocol: 'http',
+//       },
+//     ],
+//   },
+//   // The following parameters are directly passed to Typesense's search API endpoint.
+//   //  So you can pass any parameters supported by the search endpoint below.
+//   //  queryBy is required.
+//   additionalSearchParameters: {
+//     queryBy: 'title',
+//   },
+// });
+// const searchClient = typesenseInstantsearchAdapter.searchClient;
+
+const tsClient = new TypeSearchClient({
+  nodes: [
+    {
+      host: 'localhost',
+      port: '8108',
+      protocol: 'http',
+    },
+  ],
+  apiKey: 'xyz',
+  connectionTimeoutSeconds: 2,
 });
-const searchClient = typesenseInstantsearchAdapter.searchClient;
 
 function App() {
+  
   return (
     <div>
-      <header className="header">
-        <h1 className="header-title">
-          <a href="/">search-ui</a>
-        </h1>
-        <p className="header-subtitle">
-          using{' '}
-          <a href="https://github.com/algolia/react-instantsearch">
-            React InstantSearch
-          </a>
-        </p>
-      </header>
+      <Search typesenseClient={tsClient} />
+    </div>
+  );
+}
 
-      <div className="container">
-        <InstantSearch searchClient={searchClient} indexName="zettleDocuments">
-          <div className="search-panel">
-            <div className="search-panel__results">
-              <SearchBox
-                className="searchbox"
-                translations={{
-                  placeholder: '',
-                }}
-              />
-              <Hits hitComponent={Hit} />
+// Search component
+function Search(props) {
+  const [docID, setDocID] = useState(0);
+  const [searchRes, setSearchRes] = useState(null);
+  function searchReq() {
+    const search = {
+      searches: [
+        {
+          'collection': 'zettleDocuments',
+        },
+      ]
+    }
+    return search;
+  }
 
-              <div className="pagination">
-                <Pagination />
-              </div>
-            </div>
-          </div>
-        </InstantSearch>
+  function commonSearchParams(query) {
+    console.log(query)
+    let searchParams = {
+      'q': { query },
+      'query_by': 'title',
+    }
+    return searchParams;
+  }
+
+  function doSearch(typesenseClient, queryInput) {
+
+    console.log(queryInput)
+    let response = typesenseClient.multiSearch.perform(searchReq(), commonSearchParams(queryInput));
+    // this is a promise so not available when being resolved int he results section below
+    console.log("asdfs"+response)
+    return response;
+  }
+
+  return (
+    <div>
+      <div className="search-box">
+        <input
+          placeholder={props.placeholderText}
+          autoFocus={props.autoFocus}
+          className="search-box-input"
+          data-form-type=""
+          onKeyPress={(e) => {
+            setSearchRes(doSearch(props.typesenseClient, e.key))
+          }}
+        />
+        <button title="Clear search" className="search-box-clear">
+          ×
+        </button>
+      </div>
+      <div>
+
+        {searchRes && <ol className="search-results-list">
+        
+          {searchRes.hits.map(hit => (
+            <li
+              className="search-result"
+              key={hit.id}
+              onClick={() => setDocID(hit.id)}
+            >
+              <span className="search-results-title">{hit.title}</span>
+              <span className="search-result-content">{hit.content}</span>
+            </li>
+          ))}
+        </ol>
+        }
+
       </div>
     </div>
   );
 }
 
-function Hit(props) {
-  return (
-    <article>
-      <h1>
-        <Highlight attribute="title" hit={props.hit} />
-      </h1>
-      <p>
-        <Highlight attribute="content" hit={props.hit} />
-      </p>
-    </article>
-  );
-}
 
-Hit.propTypes = {
-  hit: PropTypes.object.isRequired,
-};
+
 
 export default App;
