e151ad5ad9661d60f90a2bb639df468817c69935
Janaka A <contact@janaka.co.uk>
2021-10-18 00:59:44 +0100

    highlighting across tags

--- chrome-extension/src/chromeServices/DOMEvaluator.ts
+++ chrome-extension/src/chromeServices/DOMEvaluator.ts
@@ -1,87 +1,130 @@
 import { DOMMessage, DOMMessageResponse } from '../types';
 
-document.addEventListener('mouseup',function(event)
-{
-    
+
+
+const doHighlight = function (event: MouseEvent) {
+
     let sel = window.getSelection()
     //window.alert(sel)
     console.log("mouseup event: " + sel?.toString())
 
     if (sel != null && sel.toString().length > 0) {
-        const highlight:string  = sel != null ? sel.toString() : ""
-        let pTags:HTMLCollectionOf<HTMLParagraphElement> = document.getElementsByTagName<"p">("p")
+        let highlight: string = sel != null ? sel.toString() : ""
+        let pTags: HTMLCollectionOf<HTMLParagraphElement> = document.getElementsByTagName<"p">("p")
 
-        
+
         for (let i = 0; i < pTags.length; i++) {
-            const pTag:HTMLParagraphElement = pTags[i];
-            const t:number = pTag.innerText.search(highlight)
-            console.log("highlight found " + t)
-            if(t>-1) {
-                
-                for (let j = 0; j < pTag.children.length; j++) {
-                    const child:Element = pTag.children[j];
-                    console.log(child.tagName + ": " + child.innerHTML)    
+            const pTag: HTMLParagraphElement = pTags[i];
+            const p: number = pTag.innerText.search(highlight)
+            console.log("highlight found at pos " + p)
+            if (p > -1) {
+                console.log("child node count " + pTag.childNodes.length) // includes text and commnet nodes
+                console.log("child count " + pTag.children.length) // exludes text and commnet nodes
+                console.log(pTag.innerHTML)
+
+                const s = pTag.innerHTML 
+
+                const q: number = pTag.innerHTML.search(highlight)
+
+                if (q === -1) {
+                    // finding the highlight text in the innerText but not the innerHTML means
+                    // the highlight is across a child HTML element
+                    // so include the child node HTML in the highligh text so it matches in innerHTML
+
+                    for (let j = 0; j < pTag.children.length; j++) {
+                        const child: Element = pTag.children[j];
+                        console.log(child.nodeName)
+                        if (child.nodeName === "MARK") {
+                            console.log("already highlighted. remove and rehighlight")
+                            return
+                        }
+                        const t = highlight.search(child.innerHTML)
+                        if (t > -1) { // crosses this element
+
+                            const highlighttextBeforeNode = highlight.substr(0, t)
+                            const highlighttextAfterNode = highlight.substring(t + child.innerHTML.length, highlight.length)
+                            highlight = highlighttextBeforeNode + child.outerHTML + highlighttextAfterNode
+                        }
+                    }
+
                 }
-                
 
-
-
-                const s = pTag.innerHTML //= pTag.innerText.replace(highlight, "<mark>"+highlight+"</mark>")
+                const t = highlightWithinChild(pTag, highlight)
+                if (t === "MARK") { console.log("already highlighted."); return }
 
                 const highlightStartPosition = s.indexOf(highlight)
-
                 const textBefore = s.substr(0, highlightStartPosition)
-                const textAfter = s.substring(highlightStartPosition+highlight.length, s.length)
+                const highlightEndPosition = highlightStartPosition + highlight.length
+                const textAfter = s.substring(highlightEndPosition, s.length)
+                console.log("highlight with html: " + highlight + ", highlightStartPosition=" + highlightStartPosition + ", highlightEndPosition=" + highlightEndPosition)
                 pTag.innerHTML = textBefore
                 //pTag.insertAdjacentText("beforeend", textBefore)
-                pTag.insertAdjacentHTML("beforeend","<mark>"+highlight+"</mark>")
-                pTag.insertAdjacentText("beforeend", textAfter)
+                pTag.insertAdjacentHTML("beforeend", "<mark>" + highlight + "</mark>")
+                pTag.insertAdjacentHTML("beforeend", textAfter)
+
+
             }
         }
-    
+
     }
-    
+}
 
-    // chrome.runtime.sendMessage({highlightedText: sel, greeting: "hello"}, function(response) {
-    //     console.log(response.farewell);
-    //   });
-    // if(sel.length)
-    //     chrome.extension.sendRequest({'message':'setText','data': sel},function(response){})
-})
+const undoHighlight = function(event:MouseEvent) {
+    console.log(event.)
+}
 
-console.log("hjg j")
+function highlightWithinChild(tag: Element, highlightText: string) {
+
+    let found: string = "NONE"
+
+    for (let i = 0; i < tag.children.length; i++) {
+        const e = tag.children[i];
+        const p: number = e.innerHTML.search(highlightText)
+
+        if (p > -1) {
+            found = e.nodeName
+            if (e.children.length > 0) {
+                found = highlightWithinChild(e, highlightText);
+            }
+        }
+    }
+    return found
+}
 
 // Function called when a new message is received
 const messagesFromReactAppListener = (
-   msg: DOMMessage,
-   sender: chrome.runtime.MessageSender,
-   sendResponse: (response: DOMMessageResponse) => void) => {
-  
-   console.log('[content.js]. Message received', msg);
- 
-   const headlines = Array.from(document.getElementsByTagName<"h1">("h1"))
-                       .map(h1 => h1.innerText);
- 
+    msg: DOMMessage,
+    sender: chrome.runtime.MessageSender,
+    sendResponse: (response: DOMMessageResponse) => void) => {
+
+    console.log('[content.js]. Message received', msg);
+
+    const headlines = Array.from(document.getElementsByTagName<"h1">("h1"))
+        .map(h1 => h1.innerText);
+
     // Prepare the response object with information about the site
-   const response: DOMMessageResponse = {
-       title: document.title,
-       headlines
-   };
- 
-   sendResponse(response);
+    const response: DOMMessageResponse = {
+        title: document.title,
+        headlines
+    };
+
+    sendResponse(response);
 }
- 
+
 // chrome.browserAction.onClicked.addListener(function(tab) {
 //     chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
 //        sendServiceRequest(response.data);
 //     });
 //   });
-  
+
 //   function sendServiceRequest(selectedText:any) {
 //     var serviceCall = 'http://www.google.com/search?q=' + selectedText;
 //     chrome.tabs.create({url: serviceCall});
 //   }
 
+
+document.addEventListener('mouseup', doHighlight);
+document.addEventListener('click', undoHighlight);
 
 /**
 * Fired when a message is sent from either an extension process or a content script.
