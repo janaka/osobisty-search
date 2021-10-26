e6ce8cb4a1224a553eac10a73074830cff2dadb6
Janaka Abeywardhana <contact@janaka.co.uk>
2021-10-26 19:02:33 +0100

    Clean up unused code

--- chrome-extension/src/chromeServices/highlighterContentScript.ts
+++ chrome-extension/src/chromeServices/highlighterContentScript.ts
@@ -1,18 +1,6 @@
 import { DOMMessage, DOMMessageResponse } from '../types';
 import {generateHighlightMarkup} from './utils'
 
-// const doHighlight2 = function (event: MouseEvent) {
-
-//     let sel = window.getSelection()
-
-//     console.log("mouseup event: " + sel?.toString())
-//     console.log("anchor node type: "+sel?.anchorNode?.nodeType+sel?.anchorNode?.nodeName +  ", anchor offset: " + sel?.anchorOffset)
-
-//     console.log("focus node type: "+sel?.focusNode?.nodeType + sel?.focusNode?.nodeName + ", focus offset: " + sel?.focusOffset)
-
-
-// }
-
 const doHighlight = function (event: MouseEvent) {
 
     let sel = window.getSelection()
@@ -58,76 +46,7 @@ const undoHighlight = function (event: MouseEvent) {
     //console.log(event.)
 }
 
-function topElementWithHighlightText(El: Element, highlight: string): {element: Element, position: number, hasChildren: boolean, spansElements:boolean} | null {
-    // let te = null
-    // const p: number
 
-    let highlightElement: {element: any, position: number, hasChildren: boolean, spansElements:boolean} = {element: null, position: -1, hasChildren:false, spansElements:false}
-
-    for (let k = 0; k < El.children.length; k++) {
-        const e: any = El.children[k];
-        console.log("nodename : " + e.nodeName + ", type:" + e.nodeType)
-        if (e.nodeName !== "SCRIPT") {
-            console.log(e.innerText)
-            console.log(e.innerHTML)
-            const p1  = e.innerText.indexOf(highlight)
-            if (p1 > -1) { // highlight is in this node
-                console.log("highlight found at pos " + p1)
-                const p2  = e.innerText.indexHTML(highlight)
-                if (p2>-1) { // doesn't span elements
-                    // add highligh markup
-                } else {
-
-                }
-            }
-        }
-    }
-    // for (let k = 0; k < El.children.length; k++) {
-    //     const e = El.children[k];
-    //     console.log("nodename : "+e.nodeName + ", type:" + e.nodeType)
-    // }
-    return highlightElement
-}
-// \s?<?\/?[a-zA-Z]*>?\s?
-//\(aka\s?<?\/?.*?>?\s?The\s?<?\/?.*?>?\s?Sephist\s?<?\/?.*?>?\s?\)
-
-//aka\s?<?.*?>?\s??The\s?<?.*?>?\s??Sephist\s?<?.*?>\s??the
-function spaningHighlightText(El: Element, highlight: string): string {
-    for (let j = 0; j < El.children.length; j++) {
-        const child: Element = El.children[j];
-        console.log(child.nodeName)
-        if (child.nodeName === "MARK") {
-            console.log("already highlighted. remove and rehighlight")
-            return ""
-        }
-        const t = highlight.indexOf(child.innerHTML)
-        if (t > -1) { // crosses this element
-
-            const highlighttextBeforeNode = highlight.substr(0, t)
-            const highlighttextAfterNode = highlight.substring(t + child.innerHTML.length, highlight.length)
-            highlight = highlighttextBeforeNode + child.outerHTML + highlighttextAfterNode
-        }
-    }
-    return highlight
-}
-
-function highlightWithinChild(tag: Element, highlightText: string) {
-
-    let found: string = "NONE"
-
-    for (let i = 0; i < tag.children.length; i++) {
-        const e = tag.children[i];
-        const p: number = e.innerHTML.indexOf(highlightText)
-
-        if (p > -1) {
-            found = e.nodeName
-            if (e.children.length > 0) {
-                found = highlightWithinChild(e, highlightText);
-            }
-        }
-    }
-    return found
-}
 
 // Function called when a new message is received
 const messagesFromReactAppListener = (
