b1993def72f370ac68e06e2d183cc7fa00a765df
Janaka Abeywardhana <contact@janaka.co.uk>
2021-10-30 11:40:25 +0100

    Cliping, fake save to API, and highlight working

--- chrome-extension/src/background.ts
+++ chrome-extension/src/background.ts
@@ -1,27 +1,66 @@
-import { Api, Webclipping } from './client-libs/osobisty-client'
+import { Api, Webclipping, WebClippingResponse } from './client-libs/osobisty-client'
 
-chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {    
+
+chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
   if (request.messageName === "postWebClipping") {
     console.log("Message received in background script")
     console.log("highlight: " + request.messageData)
 
-      const api = new Api()
+    const api = new Api()
 
-      const wc: Webclipping = {
-          source_content: "sdfsdfsfsadjhjkhsdfkh kjhsd fkhds f",
-          link: 'http://www.url.com'
-      }
-      const res = await api.webclippings.postWebclippings(wc)
+    const wc: Webclipping = {
+      source_content: request.messageData,
+      link: 'http://www.url.com'
+    }
 
-      if (res.error) {
-          console.error("save failed", res.error)
-
-      } else if (res.ok && res.data.webClippingData!==null) {
-          console.log(res.data.webClippingData)
-      }
-
+    api.webclippings.postWebclippings(wc)
+      .then(response => response.json())
+      .then((data: WebClippingResponse) => {
+        console.log(data.webClippingData)
+        sendResponse(data)
+      })
+      .catch((error) => { console.error(error) })
   }
 
 });
 
-export {}
+const onClickContextMenu = (onClickData: chrome.contextMenus.OnClickData, tab: any) => {
+  if (onClickData.menuItemId === "987sfhissdf343534f") {
+    // tell content script to get the selescted text. Only content scripts have access to the page DOM
+    chrome.tabs.sendMessage(tab.id, { command: "clipSelection" }, function (response) {
+      console.log(response);
+      const api = new Api()
+
+      const wc: Webclipping = {
+        source_content: response.selectedText,
+        link: response.link
+      }
+
+      api.webclippings.postWebclippings(wc)
+        .then(response => response.json())
+        .then((data: WebClippingResponse) => {
+          console.log(data.webClippingData)
+          chrome.tabs.sendMessage(tab.id, { command: "highlightSelection" }, function (response) {})
+        })
+        .catch((error) => { console.error(error) })
+    });
+  }
+}
+
+
+chrome.runtime.onInstalled.addListener(function () {
+
+  chrome.contextMenus.create({
+    id: '987sfhissdf343534f',
+    title: 'Osobisty Clip Selection',
+    type: 'normal',
+    contexts: ['selection'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(onClickContextMenu)
+
+//chrome.contextMenus.create({"title": "Osobisty Clip Selection", "contexts": ["selection"],"onclick": onClickContextMenu});
+
+
+export { }
--- chrome-extension/src/chromeServices/highlighterContentScript.ts
+++ chrome-extension/src/chromeServices/highlighterContentScript.ts
@@ -4,9 +4,9 @@ import { generateHighlightMarkup } from './utils'
 
 
 
-const doHighlight = async function (event: MouseEvent) {
+const doHighlight = async function (selectedText: string ) {
 
-    let sel = window.getSelection()
+    const sel = selectedText //window.getSelection()
 
     console.log("mouseup event: " + sel?.toString())
 
@@ -39,17 +39,14 @@ const doHighlight = async function (event: MouseEvent) {
             console.log("highlight text escaped: " + lastHightlightObj.highlightTextEscaped)
             console.log("innerHtml: " + lastHightlightObj.highlightedHtml)
         } else {
-            //save highlight
-
-
-
-            chrome.runtime.sendMessage(
-                {
-                    messageName: "postWebClipping",
-                    messageData: highlightText
-                }, (response) => {
-                    console.log(response)
-                });
+            //send message to background.js to save highlight to backend
+            // chrome.runtime.sendMessage(
+            //     {
+            //         messageName: "postWebClipping",
+            //         messageData: highlightText
+            //     }, (response) => {
+            //         console.log(response)
+            //     });
         }
 
     }
@@ -65,48 +62,64 @@ const undoHighlight = function (event: MouseEvent) {
 
 
 // Function called when a new message is received
-const messagesFromReactAppListener = (
-    msg: DOMMessage,
+// const messagesFromReactAppListener = (
+//     msg: DOMMessage,
+//     sender: chrome.runtime.MessageSender,
+//     sendResponse: (response: DOMMessageResponse) => void) => {
+
+//     console.log('[content.js]. Message received', msg);
+
+//     const headlines = Array.from(document.getElementsByTagName<"h1">("h1"))
+//         .map(h1 => h1.innerText);
+
+//     // Prepare the response object with information about the site
+//     const response: DOMMessageResponse = {
+//         title: document.title,
+//         headlines
+//     };
+
+//     sendResponse(response);
+// }
+
+const onReceiveMesssage = (
+    msg: any,
     sender: chrome.runtime.MessageSender,
-    sendResponse: (response: DOMMessageResponse) => void) => {
+    sendResponse: (response: any) => void) => {
+    
+    if (msg.command === "highlightSelection") {
+        const sel = window.getSelection()?.toString();
+        if (sel) {
+            doHighlight(sel);
+        } else {
+            console.error("There's no selection. Selected text is empty!")
+        }
+    }
 
-    console.log('[content.js]. Message received', msg);
-
-    const headlines = Array.from(document.getElementsByTagName<"h1">("h1"))
-        .map(h1 => h1.innerText);
-
-    // Prepare the response object with information about the site
-    const response: DOMMessageResponse = {
-        title: document.title,
-        headlines
-    };
-
-    sendResponse(response);
+    if (msg.command === "clipSelection") {
+        const sel = window.getSelection()?.toString();
+        if (sel) {
+            //TODO: grab url of current tab
+            sendResponse({selectedText: sel, link: window.location.href.toString()})
+        } else {
+            console.error("There's no selection. Selected text is empty!")
+        }
+    }
 }
 
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
-//        sendServiceRequest(response.data);
-//     });
-//   });
-
-//   function sendServiceRequest(selectedText:any) {
-//     var serviceCall = 'http://www.google.com/search?q=' + selectedText;
-//     chrome.tabs.create({url: serviceCall});
-//   }
 
 
-const highlightHandler = (event: MouseEvent) => {
-    console.log("sfsf")
-    doHighlight(event);
 
-}
+// const highlightHandler = (event: MouseEvent) => {
+//     console.log("sfsf")
+//     doHighlight(event);
 
-document.addEventListener('mouseup', highlightHandler);
-document.addEventListener('click', undoHighlight);
+// }
+
+//document.addEventListener('mouseup', highlightHandler);
+//document.addEventListener('click', undoHighlight);
 
 /**
-* Fired when a message is sent from either an extension process or a content script.
+* Fired when a message is received from either an extension process or a background script.
 */
-chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
-
+//chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
+chrome.runtime.onMessage.addListener(onReceiveMesssage);
