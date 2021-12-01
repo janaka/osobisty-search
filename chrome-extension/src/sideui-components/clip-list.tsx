96eb86498b4cccffd2c7216f05d498bbb554820b
Janaka Abeywardhana <contact@janaka.co.uk>
2021-12-01 01:29:08 +0000

    Fix SideUI refresh on note update - Missing curl brace around _if_ statement meant that it was gettign fired when it shouldn't

--- chrome-extension/src/sideui-components/clip-list.tsx
+++ chrome-extension/src/sideui-components/clip-list.tsx
@@ -11,7 +11,7 @@ function ClipsList() {
 
 
     window.addEventListener("message", (event: MessageEvent<any>) => {
-      console.log("SideUI received message from: " + event.data.source)
+      console.log("<SideUI><ClipsList> received message from: " + event.data.source)
       console.log(event)
       // We only accept messages from ourselves
       if (event.source !== window) {
@@ -22,55 +22,31 @@ function ClipsList() {
 
       if (event.data.source && (event.data.source === "CONTENT_SCRIPT")) {
         console.log("CONTENT_SCRIPT")
-        if (event.data.cmd && (event.data.cmd === "listHighlights"))
+        if (event.data.cmd && (event.data.cmd === "listHighlights")) {
           console.log("Received command: " + event.data.cmd + " from: " + event.data.source);
-        console.log(event.data.clippingData);
-        if (event.data.clippingData) {
-          setClipsData(event.data.clippingData)
-        } else {
-          // some test data so we can do local dev
-          const testClippingData: WebClippingData = {
-            clippings: [
-              { id: '9208749', source_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore', notes_content: 'Some notes about why I found this clip interesting.' },
-              { id: '8208749', source_content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,', notes_content: 'Some notes about why I found this clip interesting.' },
-              { id: '9608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
-              { id: '9618749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
-              { id: '7608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
-              { id: '6608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
-              { id: '2608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' }
-            ]
+          console.log(event.data.clippingData);
+          if (event.data.clippingData) {
+            setClipsData(event.data.clippingData)
+          } else {
+            // some test data so we can do local dev
+            console.log("setClipsData=testClippingData")
+            const testClippingData: WebClippingData = {
+              clippings: [
+                { id: '9208749', source_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore', notes_content: 'Some notes about why I found this clip interesting.' },
+                { id: '8208749', source_content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,', notes_content: 'Some notes about why I found this clip interesting.' },
+                { id: '9608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
+                { id: '9618749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
+                { id: '7608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
+                { id: '6608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' },
+                { id: '2608749', source_content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born ', notes_content: 'Some notes about why I found this clip interesting.' }
+              ]
+            }
+            setClipsData(testClippingData)
           }
-          setClipsData(testClippingData)
         }
         //port.postMessage(event.data.text); 
       }
     }, false);
-
-
-
-    // try {
-    //   const api = new Api()
-
-    //   const url = window.location.href
-
-    //   api.webclippings.getWebclippings({ page_url: encodeURIComponent(url) })
-    //     .then(response => response.json())
-    //     .then((data: WebClippingsResponse) => {
-    //       console.log("sideui get clips ")
-    //       console.log(data.webClippingData)
-    //       if (data.webClippingData) {
-    //         setClipsData(data.webClippingData);
-    //       } else {
-    //         throw data.message
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       console.error("<ClipsList> Error calling `api.webclippings.getWebclippings()`")
-    //       console.error(error)
-    //     })
-    // } catch (error) {
-    //   throw error
-    // }
   }, [])
 
 
@@ -82,7 +58,7 @@ function ClipsList() {
           {
             clipsData.clippings.map((clip: Model1) => (
               <li id={clip.id} key={clip.id} className="pb-4 osobisty-side-ui-result-item">
-                <Clip clipData={{clip: clip, page_url: clipsData.page_url, page_id: clipsData.id}} />
+                <Clip clipData={{ clip: clip, page_url: clipsData.page_url, page_id: clipsData.id }} />
               </li>
             ))
           }
