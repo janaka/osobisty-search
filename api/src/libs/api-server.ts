77a19d366a001a68b7573f60d991f750333df932
Janaka Abeywardhana <contact@janaka.co.uk>
2022-02-17 00:30:08 +0000

    API set cors wild card

--- api/src/libs/api-server.ts
+++ api/src/libs/api-server.ts
@@ -106,7 +106,7 @@ var hapiServerOptions: Hapi.ServerOptions = {
   tls: nodeServerOptions,
   routes: {
     cors: {
-      origin: origins,
+      origin: "*",//origins,
       headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"],
       additionalHeaders: ["Accept-language", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin: https://osobisty-search-ui.onrender.com", "Cache-Control", "Access-Control-Request-Headers", "Accept-Language", "Accept-Encoding", "Origin"], // defaults ['Accept', 'Authorization', 'Content-Type', 'If-None-Match']
     }
