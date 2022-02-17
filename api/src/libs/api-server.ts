6640c4bf4386e91081fca9a9e81079d078d675f7
Janaka Abeywardhana <contact@janaka.co.uk>
2022-02-17 00:06:55 +0000

    API Add more headers

--- api/src/libs/api-server.ts
+++ api/src/libs/api-server.ts
@@ -107,6 +107,7 @@ var hapiServerOptions: Hapi.ServerOptions = {
   routes: {
     cors: {
       origin: origins,
+      headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"],
       additionalHeaders: ["Accept-language", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin: https://osobisty-search-ui.onrender.com", "Cache-Contorl", "Access-Control-Request-Headers", "Accept-Language", "Accept-Encoding", "Origin"], // defaults ['Accept', 'Authorization', 'Content-Type', 'If-None-Match']
     }
   },
