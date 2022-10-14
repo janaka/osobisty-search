d0c4d7ddc6c28b7da2c458f66fcc9b78bcee35d5
Janaka Abeywardhana <contact@janaka.co.uk>
2022-10-14 17:47:40 +0100

    fix(editor): UL sub bullet styling to default

--- search-ui/src/components/slate-plate/plugins.ts
+++ search-ui/src/components/slate-plate/plugins.ts
@@ -42,6 +42,7 @@ import {
   PlateFloatingLink,
 } from '@udecode/plate';
 import { css } from 'styled-components';
+import tw from 'twin.macro';
 import { autoformatRules } from './autoformat/autoformatRules';
 import { linkPlugin } from './linkPlugin';
 import { createMyPlugins, MyTodoListElement } from './plateTypes';
@@ -141,83 +142,98 @@ export const PLUGINS = {
   basicElements,
   basicMarks,
   complex,
-  basicNodes: createPlugins([...basicElements, ...basicMarks], {
+  basicNodes: createMyPlugins([...basicElements, ...basicMarks], {
     components: createPlateUI(),
   },),
   // Note: 
   // styled-component css defaults are in createPlateUI.ts https://github.com/udecode/plate/blob/main/packages/ui/plate/src/utils/createPlateUI.ts
   // css` prefix is for css. tw` prefix is for TailwindCSS via twin.macro
-  allNodes: createPlugins([...basicElements, ...basicMarks, ...complex], {
-    components: createPlateUI(
-      {[ELEMENT_H1]: withProps(StyledElement, {
-        as: 'h1',
-        styles: {
-          root: css`
+  allNodes: createMyPlugins(
+    [...basicElements, ...basicMarks, ...complex],
+    {
+      components: createPlateUI({
+        [ELEMENT_PARAGRAPH]: withProps(StyledElement, {
+          // as: 'p',
+          styles: {
+            root: {
+              margin: 0,
+              padding: '4px 0',
+            },
+          },
+          prefixClassNames: 'p',
+        }),
+        [ELEMENT_H1]: withProps(StyledElement, {
+          as: 'h1',
+          styles: {
+            root: css`
             margin: 0.15em 0 0.5em;
             font-size: 2em;
             font-weight: 700;
             line-height: 1.3;
             color: #666666;
             `,
-        },
-      }),
-      [ELEMENT_H2]: withProps(StyledElement, {
-        as: 'h2',
-        styles: {
-          root: css`
+          },
+        }),
+        [ELEMENT_H2]: withProps(StyledElement, {
+          as: 'h2',
+          styles: {
+            root: css`
             margin: 0.15em 0 0.5em;
             font-size: 1.5em;
             font-weight: 600;
             line-height: 1.3;
             `,
-        },
-      }),
-      [ELEMENT_H3]: withProps(StyledElement, {
-        as: 'h3',
-        styles: {
-          root: css`
+          },
+        }),
+        [ELEMENT_H3]: withProps(StyledElement, {
+          as: 'h3',
+          styles: {
+            root: css`
             margin: 0.15em 0 0.5em;
             font-size: 1em;
             font-weight: 700;
             line-height: 1.3;
             color: #666666;
             `,
-        },
-      }),
-      [ELEMENT_UL]: withProps(StyledElement, {
-        as: 'ul',
-        styles: {
-          root: css`          
+          },
+        }),
+        [ELEMENT_UL]: withProps(StyledElement, {
+          as: 'ul',
+          styles: {
+            root: [
+              tw``,
+              css`
+          list-style: revert;
+          margin: 0;
+          padding-inline-start: 20px;
+        `,]
+          },
+        }),
+        [ELEMENT_OL]: withProps(StyledElement, {
+          as: 'ol',
+          styles: {
+            root: css`
           margin: 0;
           padding-inline-start: 20px;
         `,
-        },
-      }),
-      [ELEMENT_OL]: withProps(StyledElement, {
-        as: 'ol',
-        styles: {
-          root: css`
-          margin: 0;
-          padding-inline-start: 20px;
-        `,
-        },
-      }),
-      // [ELEMENT_LI]: withProps(StyledElement, {
-      //   as: 'li',
-      //   styles: {
-      //     root: [css`list-style:initial;`]
-      //   },
-      // }),
-      // [ELEMENT_LINK]: withProps(StyledElement, {
-      //   as: 'a',
-      //   styles: {
-      //     root: [tw`text-blue-600 visited:text-purple-600`,css``]
-      //   },
-      // }),
+          },
+        }),
+        // [ELEMENT_LI]: withProps(StyledElement, {
+        //   as: 'li',
+        //   styles: {
+        //     root: [css`list-style:initial;`]
+        //   },
+        // }),
+        [ELEMENT_LINK]: withProps(StyledElement, {
+          as: 'a',
+          styles: {
+            root: [tw`text-blue-600 visited:text-purple-600`,css``]
+          },
+        }),
 
-    }
-    ),
-  },),
+      }
+      ),
+    },),
 
   // image: createPlugins(
   //   [
